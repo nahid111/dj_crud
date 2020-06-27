267fe49688eef21d86ba38b5d07161038eb5adb7
Muhammad Nahid <mdnahid22@gmail.com>
2020-06-27 11:08:38 +0600

    create app

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('<h1>Hello World</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>About Page</h1>')
+
+
