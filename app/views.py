df91c979eb6b38c53e2457391ed46991ee520ab3
Muhammad Nahid <mdnahid22@gmail.com>
2020-06-27 22:53:14 +0600

    templates, static

--- app/views.py
+++ app/views.py
@@ -1,11 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request, 'index.html')
 
 
 def about(request):
-    return HttpResponse('<h1>About Page</h1>')
+    return render(request, 'about.html')
 
 
